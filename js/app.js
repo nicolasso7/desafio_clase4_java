3f6301c9fc831df3c314494af49ca8a86c4923cb
nicolasso <nicolassocac@gmmail.com>
2021-09-29 23:43:03 -0300

    modificaciones

--- js/app.js
+++ js/app.js
@@ -1,16 +1,42 @@
-let color = prompt("Ingresa color de Parlante");
+let precioCosto = parseFloat (prompt("Ingrese el precio del costo del parlante"));
 
-    switch ((parlante = color)) {
-        case "negro":
-        alert("hay en stock");
+while (true){
+    if (!isNaN(precioCosto) && precioCosto != null &&
+    precioCosto !="") {
         break;
-        case "blanco":
-        alert("no hay stock");
-        break;
-        case "color madera":
-        alert("ultimo en stock");
-        break;
-        default:
-           alert("ESE COLOR NO ESTA EN STOCK")
-
+    } else{
+        alert("no es un numero")
     }
+    continue;
+}
+//SALIDA
+precioCostoM = precioCosto.toFixed(2)
+alert("El Costo del parlante es: " + precioCostoM);
+console.log("El Costo del parlante es: " +precioCostoM);
+document.write ("<h2>El costo del parlante es: "+precioCostoM+"</h2>")
+
+// SUMAR EL IVA AL PRODUCTO
+function CalcularIva(precioCosto){
+    return (precioCosto * 0.21);
+}
+
+let iva = CalcularIva(precioCosto);
+ivaM = iva.toFixed(2);
+
+alert("El IVA del parlante es: " + ivaM);
+console.log("El IVA del parlante es: " +ivaM);
+document.write ("<h2>El IVA del parlante es: "+ ivaM +"</h2>");
+
+// SUMAR COSTO + IVA
+
+function sumarIva(precioCosto){
+    return (precioCosto + precioCosto * 0.21);
+}
+
+let precioConIva = sumarIva (precioCosto);
+precioConIvaM = precioCostoIva.toFixed(2);
+
+alert("El parlante con IVA es: " + precioConIvaM);
+console.log("El parlante con IVA es: " + precioConIvaM);
+document.write ("<h2>El parlante con IVA es: " + 
+precioConIvaM + "</h2>");
